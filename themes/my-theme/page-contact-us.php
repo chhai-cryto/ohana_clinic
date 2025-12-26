d628f5c47d878d2a80425f7e5e6a8f6d1381ee55
chhai-cryto <chhai.dev@gmial.com>
2025-12-26 10:47:31 +0700

    k2

--- themes/my-theme/page-contact-us.php
+++ themes/my-theme/page-contact-us.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">  
-     <title>contact</title>
-</head>
-<body>
+<?php get_header(); ?>
     <!-- contact-us -->
      <div class=" contact-us">
         <p> <span >Home </span>/ Contact Us</p>
